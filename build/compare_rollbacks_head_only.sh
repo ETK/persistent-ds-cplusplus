f8851c133a6b623bdc82bebcb27454fb5f2c1f80
Sune Keller <sune.keller@gmail.com>
2012-12-07 16:07:07 +0100

    Updated scripts to use new program arguments

--- build/compare_rollbacks_head_only.sh
+++ build/compare_rollbacks_head_only.sh
@@ -5,13 +5,13 @@ date +%s%N > begin_compare_rollbacks_head_only
 for c in 1000 2327  5415  12599 29317 68219 158740 369375  859506  2000000
 do
   echo "Count: ${c}"
-  for i in -p -r -l
+  for i in -n -b -e
   do
     echo "Implementation ${i}"
     for k in 1 2 3 4 5 6 7 8 9 10
     do
       echo "Run #${k}"
-      ./msc $i -s -c $c --randomize-operations -h
+      ./msc $i -s -c $c -r -h --seed $(date +%N)
     done
   done
 done
